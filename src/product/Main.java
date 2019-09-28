7491872bf9d210caf9ac08d07aa8f3586b6fe298
SamKT17 <skthomas0339@eagle.fgcu.edu>
2019-09-28 11:40:12 -0400

    Added some style to the project.

--- src/product/Main.java
+++ src/product/Main.java
@@ -23,7 +23,7 @@ public class Main extends Application {
   public void start(Stage primaryStage) throws Exception {
 
     Parent root = FXMLLoader.load(getClass().getResource("ProLineGUI.fxml"));
-    primaryStage.setTitle("Product Line Tracker");
+    primaryStage.setTitle("Production Tracker");
     primaryStage.setScene(new Scene(root, 600, 400));
     primaryStage.show();
   }
