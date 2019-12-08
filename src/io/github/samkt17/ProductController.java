0d3a2e8b3621493b4b18cfd2e6b5cc8288ebcec8
SamKT17 <skthomas0339@eagle.fgcu.edu>
2019-12-07 19:56:39 -0500

    Added JavaDocs to docs folder.

--- src/io/github/samkt17/ProductController.java
+++ src/io/github/samkt17/ProductController.java
@@ -7,8 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
